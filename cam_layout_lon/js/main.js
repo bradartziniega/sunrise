f9d4c278beaa834cef9af9b2cb5a0d488dff6a11
bradsimpson <bradley.simpson@gmail.com>
2014-02-09 17:27:58 -0800

    trying to even out longitudinal

--- cam_layout_lon/js/main.js
+++ cam_layout_lon/js/main.js
@@ -1,28 +1,52 @@
 $(document).ready(function() {
 	
-  initializeGrid();
 
 	var cur_item = 0;
   var output_object = [];
+  
+  initializeGrid();
+
+ 
 
 });
 
 function initializeGrid(){
 
   $.getJSON( "http://localhost/~bsimpson/sunrise/cam_layout_lon/cam_to_use.json", function( data ) {
+    
     var sort_array = [];
 
     $.each( data, function( key, val ) {
-      sort_array.push({key:key,lon:val['data']['lon'],to_use:val['to_use']})
+      if(val['to_use']=='good'){
+        sort_array.push({key:key,lon:val['data']['lon'],to_use:val['to_use']})
+      }
     });
 
     sort_array.sort(function(x,y){return x.lon - y.lon})
+    
+    var numCams = 0;
+    var lonSum = 100;
+    for( var i=1;i<sort_array.length;i++){
 
-    for( var i=0;i<sort_array.length;i++){
-      console.log(data[sort_array[i].key]);
+      if(i>0){
+        
+        var diff = Math.abs(data[sort_array[i].key]['data']['lon'] - data[sort_array[i-1].key]['data']['lon']);
+        lonSum+=diff;
+
+        if(lonSum>=3){
+          console.log(lonSum);
+          lonSum=0;
+          $('.container').append($('<div>').load(data[sort_array[i].key]['data']['htmlfile']));
+          console.log(data[sort_array[i].key]['data']['htmlfile']);
+          numCams++;
+        } 
+      }  
     }
 
+    $('.container').css({ width: numCams*960});
+
   });
+
 
 
 }
