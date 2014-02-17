3c357a4e437efbe4984778d41ee42bdce7c53ab1
bradsimpson <bradley.simpson@gmail.com>
2014-02-17 09:55:13 -0800

    map markers added

--- cam_layout_lon/js/main.js
+++ cam_layout_lon/js/main.js
@@ -1,9 +1,18 @@
+var map;
+
 $(document).ready(function() {
 	
 
 	var cur_item = 0;
   var output_object = [];
   
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8
+  };
+
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
   initializeGrid();
 
 
@@ -28,6 +37,7 @@ function initializeGrid(){
   //$.getJSON( "cam_to_use.json", function( data ) {
   
     var sort_array = [];
+    var cameras_added = [];
 
     $.each( data, function( key, val ) {
       if(val['to_use']=='good'){
@@ -53,6 +63,7 @@ function initializeGrid(){
           
           $('.container').append($('<div>').load(data[sort_array[i].key]['data']['htmlfile']));
           $('.name_container').append($('<div class="cam_name">').html(data[sort_array[i].key]['data']['lat'] + ', ' + data[sort_array[i].key]['data']['lon']));
+          cameras_added.push({lon:data[sort_array[i].key]['data']['lat'],lat:data[sort_array[i].key]['data']['lon']});
           numCams++;
 
         } 
@@ -61,6 +72,17 @@ function initializeGrid(){
 
     $('.container').css({ width: numCams*960});
     $('.name_container').css({ width: numCams*960});
+
+    for (var i=0;i<cameras_added.length;i++){
+        var marker = new google.maps.Marker({
+             position: new google.maps.LatLng(cameras_added[i]['lon'],cameras_added[i]['lat']),
+             map: map,
+             zIndex: i+1
+         });
+         console.log(cameras_added[i]['lat']);
+
+
+    }
 
   });
 
@@ -71,30 +93,6 @@ function initializeGrid(){
 
 function initialize() {
 
-  var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8
-  };
-
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-
-    var hotels = [
-        ['ibis Birmingham Airport', 52.452656, -1.730548, 4],
-        ['ETAP Birmingham Airport', 52.452527, -1.731644, 3],
-        ['ibis Birmingham City Centre', 52.475162, -1.897208, 2]
-        ];
-
-     for (var i = 0; i < hotels.length; i++) {
-         var marker = new google.maps.Marker({
-             position: new google.maps.LatLng(hotels[i][1], hotels[i][2]),
-             map: map,
-           //  icon: image,
-             title: hotels[i][0],
-             zIndex: hotels[i][3]
-         });
-         console.log(hotels[i][0]);
-     }
-
+ 
 }
 
